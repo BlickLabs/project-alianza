7498131e9d9ae8d001c575d83ff1858c3ca86e3d
Raúl Marty <santor3medio@gmail.com>
2017-12-12 17:51:48 -0600

    #45. Modify name of objects in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,13 @@
 $('form[name="alianza-contact-form"]').validate({
   // required fields
-  alianzacontactname: 'required',
-  alianzacontactphone: 'required',
-  alianzacontactemail: 'required',
-  alianzacontactmessage: 'required',
-  alianzacontactcompany: 'required',
+  name: 'required',
+  phone: 'required',
+  email: 'required',
+  message: 'required',
   messages: {
-   alianzacontactname: "Por favor, introduce tu Nombre",
-   alianzacontactphone: 'Por favor, introduce tu Teléfono',
-    alianzacontactemail: 'Por favor introduce un Correo Electrónico',
-    alianzacontactmessage: 'Por favor introduce tu Mensaje',
-    alianzacontactcompany: 'Por favor selecciona una Opción'
+   name: "Por favor, introduce tu Nombre",
+   phone: 'Por favor, introduce tu Teléfono',
+    email: 'Por favor introduce un Correo Electrónico',
+    message: 'Por favor introduce tu Mensaje',
   }
 });
