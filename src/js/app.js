0637192b828e9671693aa4cac321088cacb73427
Raúl Marty <santor3medio@gmail.com>
2017-12-13 10:37:37 -0600

    Modify app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+
 $('form[name="alianza-contact-form"]').validate({
   // required fields
   name: 'required',
@@ -5,9 +6,29 @@ $('form[name="alianza-contact-form"]').validate({
   email: 'required',
   message: 'required',
   messages: {
-   name: "Por favor, introduce tu Nombre",
-   phone: 'Por favor, introduce tu Teléfono',
-    email: 'Por favor introduce un Correo Electrónico',
-    message: 'Por favor introduce tu Mensaje',
+   name: "Por favor, introduce tu Nombre.",
+   phone: "Por favor, introduce tu Teléfono.",
+   email: "Por favor, introduce un Correo Electrónico.",
+   message: "Por favor, introduce tu Mensaje",
+  },
+  submitHandler: function(form) {
+   var data = $('form[name="alianza-contact-form"]').serialize();
+   console.log(data);
+   $.ajax({
+     url: 'http://integrations.blick.mx/alianza/contact/',
+     method: 'POST',
+     data: data
+   }).done(function(data) {
+     if (parseInt(data) === 1) {
+       alertify.logPosition("bottom right");
+       alertify.success("Correo enviado, gracias por contactarte con nosotros.");
+       $('form[name="alianza-contact-form"]')[0].reset();
+     }
+   });
+  },
+  invalidHandler: function(event, validator) {
+   var errors = validator.numberOfInvalids();
+   alertify.logPosition("bottom right");
+   alertify.error("Verifica tu información tienes " + errors + " errores.");
   }
 });
