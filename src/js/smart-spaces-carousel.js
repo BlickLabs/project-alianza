ba46f7024e0be3a95fe6c5ab2162ce5b91c59386
Raúl Marty <santor3medio@gmail.com>
2017-12-18 13:04:01 -0600

    #54. Create js file smart-spaces-carousel.js

---
+++ src/js/smart-spaces-carousel.js
@@ -0,0 +1,20 @@
+$('.smart-spaces-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  dots: false,
+  center: true,
+  autoWidth: false,
+  navText: ["<img class='owl-nav-icon' src='img/icons/left-arrow.png'>", "<img class='owl-nav-icon' src='img/icons/right-arrow.png'>"],
+  responsive:{
+    0:{
+        items:1
+    },
+    600:{
+        items:3
+    },
+    1000:{
+        items:5
+    }
+  }
+});
